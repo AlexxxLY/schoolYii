1285a852cc1726961755e85302813bf5a52dd774
Alex Ly <alexxx.ax4@gmail.com>
2018-10-16 14:41:32 +0300

    add join login views +htaccess

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,3 +1,10 @@
+<?php
+
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+?>
+<?php $this->beginPage(); ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,9 +13,41 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>VideoSchool</title>
+    <?php $this->head(); ?>
+
 </head>
 <body>
-<?= $content ?>
+
+<?php $this->beginBody(); ?>
+
+<?php
+
+    NavBar::begin([
+         'brandLabel' => 'VideoSchool',
+         'brandUrl' => Yii::$app->homeUrl,
+         'options' => [
+                 'class' => 'navbar-default navbar-fixed-top'
+         ]
+    ]);
+    $menu = [
+      ['label' => 'Join',  'url' => ['site/join']],
+      ['label' => 'Login', 'url' => ['site/login']]
+    ];
+    echo Nav::widget([
+            'options' => ['class' => 'navbar-nav navbar-right'],
+            'items' => $menu
+        ]
+    );
+    NavBar::end();
+
+?>
+<div class="container" style="margin-top: 70px">
+    <?= $content ?>
+</div>
+
+<?php $this->endBody(); ?>
 
 </body>
 </html>
+
+<?php $this->endPage(); ?>
