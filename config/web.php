28481185a4604edd541833cbdbca23fda0a47d9a
Alex Ly <alexxx.ax4@gmail.com>
2018-10-16 18:24:31 +0300

    add debug

--- config/web.php
+++ config/web.php
@@ -3,11 +3,21 @@
 return [
     'id' => 'school',
     'basePath' => realpath(__DIR__ . '/../'),
+    'bootstrap' =>[
+        'debug'
+    ],
     'components' => [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false
+        ],
+        'request' => [
+            'cookieValidationKey' => 'super secret code'
         ]
 
+    ],
+    'modules' => [
+        'debug' => 'yii\debug\Module'
     ]
+
 ];
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,13 +3,18 @@
 
 namespace app\controllers;
 
-
+use yii;
 use yii\web\Controller;
 
 class SiteController extends Controller
 {
     public function actionIndex()
     {
+        Yii::trace("I'm here", "site");
+        Yii::info("some information", "site");
+        Yii::warning("low memory", "site");
+        Yii::error("Fatall","site");
+
         return $this->render('index');
     }
     public function actionJoin()
